debae180ccdfdc8f29c274095801d9e5b0fb0e0d
Colin Dumitru <colin.dumitru@gmail.com>
2013-10-14 10:22:45 +0300

    minor change to options array

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,10 +2,35 @@ var options = {
     'listsBoards' : {
         label: 'List Boards',
         template: ''
+    },
+    'newBoard' : {
+        label: 'New Board',
+        template: ''
     }
 }
 
-
-function popupaleOptions() {
-
+function handleRequestSelect() {
+    var us = $(this);
+    var option = us.val();
 }
+
+function populateOptions() {
+    var requestsSelect = $("#requestsSelect");
+
+    for (var option in options) {
+
+        if(options.hasOwnProperty(option)) {
+            var elem = $(document.createElement('option'));
+
+            elem
+                .attr('value', option)
+                .text(options[option].label);
+
+            requestsSelect.append(elem);
+        }
+    }
+
+    requestsSelect.change(handleRequestSelect);
+}
+
+$(document).ready(populateOptions);
